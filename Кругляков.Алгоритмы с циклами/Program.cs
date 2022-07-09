ddf7709c886042d1bee10bdd7877bf01da8535b1
Alexander <Alexander@WIN-SJJ63O1G4UQ>
2022-07-10 02:14:39 +0300

    Добавьте файлы проекта.

---
+++ Кругляков.Алгоритмы с циклами/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Кругляков.Алгоритмы_с_циклами
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
